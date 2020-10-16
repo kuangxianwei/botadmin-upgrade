5b40342c19abfef9454b9889e14e304198d6dc40
kuangxianwei <38050123@qq.com>
2020-10-16 12:56:27 +0800

    update

--- after.sh
+++ after.sh
@@ -1,3 +1,2 @@
 # ${RootPath} 程序根目录
-rm -rf "${RootPath}/data/template/empirecms/博客形式"
 rm -rf "${RootPath}/.gitattributes"
--- shell/install.sh
+++ shell/install.sh
@@ -29,24 +29,100 @@ Download_botadmin() {
 #写入脚本#
 Write_botadmin() {
   #开机启动本程序#
-  cat >"/etc/systemd/system/botadmin.service" <<EOF
-[Unit]
-Description=botadmin
-After=network.target remote-fs.target nss-lookup.target mysql.service
+  cat >"/etc/init.d/botadmin" <<EOF
+#!/bin/sh
+#
+# botadmin - this script starts and stops the botadmin daemon
+#
+# chkconfig:   - 85 15
+# description: BotAdmin system
+# processname: botadmin
+# Url http://www.botadmin.cn
+# Last Updated 2020.06.01
 
-[Service]
-Type=forking
-ExecStart=$APP fork
-ExecStop=$APP stop
-ExecReload=$APP restart
-Restart=no
-PrivateTmp=false
+# Source function library.
+. /etc/rc.d/init.d/functions
 
-[Install]
-WantedBy=multi-user.target
+# Source networking configuration.
+. /etc/sysconfig/network
+
+# Check that networking is up.
+[ "\$NETWORKING" = "no" ] && exit 0
+# 程序名称
+appName="botadmin"
+# 程序目录
+appDir="$APP_DIR"
+# 程序路径
+appPath="\${appDir}/\${appName}"
+# 锁文件
+lockfile="/var/lock/subsys/\${appName}"
+prog=\$appName
+cd \$appDir || exit 0
+
+start() {
+  echo -n \$"Starting \$prog: "
+  \$appPath &
+  echo_success
+  retval=\$?
+  echo
+  [ \$retval -eq 0 ] && touch \$lockfile
+  return \$retval
+}
+stop() {
+  echo -n \$"Stopping \$prog: "
+  killproc "\$appName" >/dev/null 2>&1
+  echo_success
+  retval=\$?
+  echo
+  [ \$retval -eq 0 ] && rm -f \$lockfile
+  return \$retval
+}
+
+restart() {
+  stop
+  start
+}
+
+uninstall() {
+  stop
+  rm -fr \$appDir
+  rm -f /etc/rc.d/init.d/\${appName}
+}
+
+rh_status() {
+  status \$prog
+}
+
+rh_status_q() {
+  rh_status >/dev/null 2>&1
+}
+
+case "\$1" in
+start)
+  rh_status_q && exit 0
+  \$1
+  ;;
+stop)
+  rh_status_q || exit 0
+  \$1
+  ;;
+restart)
+  \$1
+  ;;
+status)
+  rh_status
+  ;;
+uninstall)
+  \$1
+  ;;
+*)
+  echo \$"Usage: \$0 {start|stop|status|restart}"
+  exit 2
+  ;;
+esac
 EOF
   [ $? -eq 0 ] || exit 1
-  chmod 644 /etc/systemd/system/botadmin.service
+  chmod +x /etc/init.d/botadmin
   test -d "${APP_DIR}/data" || mkdir -p "${APP_DIR}/data"
   cat >"${APP_DIR}/data/.my.cnf" <<EOF
 [client]
