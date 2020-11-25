0c3016ba0861d696cddb83544b50c8175b30998c
kuangxianwei <38050123@qq.com>
2020-11-25 13:48:33 +0800

    del

--- data/template/dedecms/22/skin/js/jquery.superslide.js
+++
@@ -1,2 +0,0 @@
-document.write("<script language='javascript' src='/skin/js/slide.js'><\/script>");
-
--- data/template/dedecms/44/js/61000000004854.js
+++
@@ -1,1 +0,0 @@
-document.write("<a target='_blank'  href='http://wljg.snaic.gov.cn/wljgweb/bscx.do?method=hddoc&id=61000000004854'><img src='images/i_lo2.gif' border='0'></a>");
