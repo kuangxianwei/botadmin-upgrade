69a1230fbc49d1c1d5cf268bc790081fcca20540
kuangxianwei <38050123@qq.com>
2021-03-02 09:03:05 +0800

    update

--- after.sh
+++ after.sh
@@ -1,4 +1,3 @@
 # ${RootPath} 程序根目录
 rm -rf "${RootPath}/.gitattributes"
-rm -rf "${RootPath}/templates/site/add.html"
-rm -rf "${RootPath}/templates/site/modify.html"
+rm -rf "${RootPath}/data/ip2region.db"
