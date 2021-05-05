08134819d899fefbd324b304a81b7e4783b4fbdf
kuangxianwei <38050123@qq.com>
2021-05-05 08:43:31 +0800

    update

--- before.sh
+++ before.sh
@@ -1,2 +1,13 @@
 # ${RootPath} 程序根目录
 # rm -rf ${RootPath}/data/template/empirecms
+rm -rf ${RootPath}/controller/collect.go \
+${RootPath}/controller/pushAd.go \
+${RootPath}/data/template/empirecms/sys_blog-4/e1618119791.temp \
+${RootPath}/models/collect.go \
+${RootPath}/models/pushAd.go \
+${RootPath}/static/layui/iconfont/iconfont.eot \
+${RootPath}/static/layui/iconfont/iconfont.svg \
+${RootPath}/templates/collect/index.tpl \
+${RootPath}/templates/pushad/common.tpl \
+${RootPath}/templates/pushad/exclude.tpl \
+${RootPath}/templates/pushad/index.tpl
