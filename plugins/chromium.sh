3211f4415f5a50b22d73803dd6cba11469fb37d8
kuangxianwei <38050123@qq.com>
2021-07-16 11:04:54 +0800

    update

--- plugins/chromium.sh
+++ plugins/chromium.sh
@@ -18,20 +18,6 @@ Install() {
     ipa-gothic-fonts xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc; then
     Error "安装Chromium依赖库失败"
   fi
-  return 
-  # 依赖库
-  for plugin in pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64; do
-    if ! yum install -y "$plugin"; then
-      Error "安装依赖库 $plugin 失败"
-    fi &
-  done
-  # 字体
-  for plugin in ipa-gothic-fonts xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc; do
-    if ! yum install -y "$plugin"; then
-      Error "安装字体库 $plugin 失败"
-    fi &
-  done
-  wait
 }
 # 卸载
 Uninstall() {
@@ -39,21 +25,6 @@ Uninstall() {
     ipa-gothic-fonts xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc; then
     Error "卸载Chromium依赖库失败"
   fi
-
-  return
-  # 依赖库
-  for plugin in pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64; do
-    if ! yum remove -y "$plugin"; then
-      Error "卸载依赖库 $plugin 失败"
-    fi &
-  done
-  # 字体
-  for plugin in ipa-gothic-fonts xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc; do
-    if ! yum remove -y "$plugin"; then
-      Error "卸载字体库 $plugin 失败"
-    fi &
-  done
-  wait
 }
 
 # 执行安装或卸载
