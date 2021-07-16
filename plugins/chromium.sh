6667f87a7012a3a0d0af3c4c42dcd293f4c68feb
kuangxianwei <38050123@qq.com>
2021-07-16 10:59:32 +0800

    update

--- plugins/chromium.sh
+++ plugins/chromium.sh
@@ -30,6 +30,15 @@ Install() {
 }
 # 卸载
 Uninstall() {
+  if ! yum remove -y pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64 \
+  ipa-gothic-fonts xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc;then
+    Error "卸载Chromium依赖库失败"
+    fi
+
+
+  
+
+  return
   # 依赖库
   for plugin in pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64; do
     if ! yum remove -y "$plugin"; then
