db3cfa6c474a3a2449656347cb9eb41a3d7cd842
kuangxianwei <38050123@qq.com>
2021-12-11 08:46:04 +0800

    update

--- static/modules/step.js
+++ static/modules/step.js
@@ -692,8 +692,8 @@ layui.define(['form', 'trans', 'rules', 'detail', 'main'], function (exports) {
                     url: '/spider/test/list',
                     data: obj.field,
                     ending: function () {
-                        main.ws.log("spider_test_list.0", function (dom) {
-                            console.log(dom);
+                        main.ws.log("spider_test_list.0", function () {
+                            console.log("textarea:",$("textarea").val());
                         });
                         return false;
                     },
